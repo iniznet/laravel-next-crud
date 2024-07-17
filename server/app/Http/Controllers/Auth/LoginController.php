08f46e6d19e2896785fcc55cd9bfe03b990b4186
iniznet <iniznet@gmail.com>
2024-07-17 11:52:48 +0700

    feat(laravel): authentication api

---
+++ server/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function __invoke(LoginRequest $request)
+    {
+        $request->authenticate();
+        $request->session()->regenerate();
+
+        return response()->json(Auth::user());
+    }
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('api')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('register', RegisterController::class);
+        Route::post('login', LoginController::class);
+        Route::post('logout', LogoutController::class);
+    });
+});
