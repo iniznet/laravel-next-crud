639b8ac9e0c1f5b39314ddb08157a99d821e230b
iniznet <iniznet@gmail.com>
2024-07-25 16:48:43 +0700

    fix: sidebar not rendered

--- client/app/(main)/layout.tsx
+++ client/app/(main)/layout.tsx
@@ -1,33 +1,16 @@
-// client/app/(main)/layout.tsx
 'use client';
 
 import React, { ReactNode, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { AuthProvider, useAuth } from '../../contexts/AuthContext'; // Adjust the import path as needed
+import Layout from '@/layout/layout';
 
 interface AppLayoutProps {
     children: React.ReactNode;
 }
 
-const DashboardLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const { isAuthenticated, loading } = useAuth();
-    const router = useRouter();
-
-    useEffect(() => {
-        if (!loading && !isAuthenticated) {
-            router.push('/auth');
-        }
-    }, [loading, isAuthenticated, router]);
-
-    if (loading) return <div>Loading...</div>;
-
-    return <>{children}</>;
-};
-
 export default function AppLayout({ children }: AppLayoutProps) {
-    return (
-        <AuthProvider>
-            <DashboardLayout>{children}</DashboardLayout>
-        </AuthProvider>
-    );
+    return <AuthProvider>
+        <Layout>{children}</Layout>
+    </AuthProvider>;
 }
